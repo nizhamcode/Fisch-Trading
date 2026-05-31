5f5af7afa1eca8602adfb81777b23cf3ed3a9e77
Abdullah Nizham Dev <zimzam.id@gmail.com>
2026-05-31 17:17:53 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,113 @@
+
+let cart = [];
+
+function addToCart(name, price){
+
+    cart.push({
+        name:name,
+        price:Number(price)
+    });
+
+    renderCart();
+
+    localStorage.setItem(
+        "zimzamCart",
+        JSON.stringify(cart)
+    );
+
+    alert(name + " added to cart!");
+}
+
+function renderCart(){
+
+    let html = "";
+    let total = 0;
+
+    cart.forEach(item=>{
+
+        html += `
+        <p>
+            ${item.name}
+            - $${item.price}
+        </p>
+        `;
+
+        total += item.price;
+    });
+
+    const items =
+    document.getElementById("cart-items");
+
+    const totalText =
+    document.getElementById("cart-total");
+
+    const count =
+    document.getElementById("cart-count");
+
+    if(items){
+        items.innerHTML = html;
+    }
+
+    if(totalText){
+        totalText.innerText =
+        total.toFixed(2);
+    }
+
+    if(count){
+        count.innerText =
+        cart.length;
+    }
+}
+
+function checkout(){
+
+    if(cart.length === 0){
+
+        alert("Cart is empty");
+        return;
+    }
+
+    let total = 0;
+
+    let text =
+`Hello Zimzam Trading
+
+I want to order:
+
+`;
+
+    cart.forEach(item=>{
+
+        text +=
+`• ${item.name} - $${item.price}
+`;
+
+        total += item.price;
+    });
+
+    text +=
+`
+Total = $${total.toFixed(2)}
+
+Thank you.
+`;
+
+    window.open(
+        `https://wa.me/628881010901?text=${encodeURIComponent(text)}`,
+        "_blank"
+    );
+}
+
+window.onload = function(){
+
+    const saved =
+    localStorage.getItem("zimzamCart");
+
+    if(saved){
+
+        cart =
+        JSON.parse(saved);
+
+        renderCart();
+    }
+};
